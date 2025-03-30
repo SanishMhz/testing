23863a274930cf06e1e7e724c941857afafc1b44
rubeshresthaa <rubesh.shrestha619@gmail.com>
2025-03-30 11:58:23 +0545

    add missing content

--- components/others/WhoSection.tsx
+++ components/others/WhoSection.tsx
@@ -4,6 +4,7 @@ const WhoSection = () => {
   return (
     <div className="py-5">
       <h1 className="text-2xl lg:text-3xl text-[#5c2653] font-semibold text-center">Who We Are?</h1>
+      {/* Business Story */}
       <div className="flex flex-col md:flex-row justify-center px-2 md:px-10 lg:px-20 py-4 gap-5">
         {/* First Section */}
         <div className="flex-2 space-y-3">
@@ -18,11 +19,29 @@ const WhoSection = () => {
           width={0}
           height={0}
           sizes="100vw"
-          className="w-full lg:w-96 h-72 rounded-2xl"
+          className="w-full lg:w-96 h-72 object-cover rounded-2xl"
            />
         </div>
       </div>
-     
+     {/* Background Story */}
+     <div className="flex flex-col md:flex-row justify-center items-center gap-5 px-5 md:px-10 lg:px-24">
+      {/* First Section */}
+      <div className="w-full lg:w-1/2">
+        <Image
+        src="/meditation.jpg"
+        alt="Meditation"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full md:w-96 h-96 object-cover"
+        />
+      </div>
+      {/* Second Section */}
+      <div className="space-y-2">
+        <h1 className="text-xl lg:text-2xl text-[#5c2653] font-semibold">Business/Brand Story</h1>
+        <p className="text-justify text-gray-700">The founder, Mr. Amit Shrestha, is a dedicated Sound Healing Master with over 9 years of experience and 21 years of practice in Reiki healing. His sessions and teaching approach blend ancient healing methods with scientific reasoning. With a deep passion for music, his sessions are harmoniously tailored to suit the needs of various individuals.</p>
+      </div>
+     </div>
     </div>
   )
 }
