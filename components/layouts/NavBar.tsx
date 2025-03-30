5566c7cc47292f662bcafeea297366d16b8dc5b8
Raju <rajutamang3456@gmail.com>
2025-03-30 12:27:39 +0545

    fix: merge 3

--- components/layouts/NavBar.tsx
+++ components/layouts/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link"
+import Link from "next/link";
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { RiMenuFill } from "react-icons/ri";
@@ -13,32 +13,64 @@ const NavBar = () => {
     <div className="bg-[#dfc4db] py-4 text-gray-600 relative ">
       {/* Larger Screen */}
       <div className="hidden md:flex justify-evenly items-center gap-5 font-bold text-xl px-72">
-        <Link href={"#"} className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl">Home</Link>
-        <Link href={"#"} className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl">Who We Are?</Link>
-        <Link href={"#"} className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl">Service</Link>
-        <Link href={"#"} className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl">Success Stories</Link>
-        <Link href={"#"} className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl">Let's Connect</Link>
-        <Link href={"#"} className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl">Login/Register</Link>
+        <Link
+          href={"#"}
+          className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl"
+        >
+          Home
+        </Link>
+        <Link
+          href={"#"}
+          className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl"
+        >
+          Who We Are?
+        </Link>
+        <Link
+          href={"#"}
+          className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl"
+        >
+          Service
+        </Link>
+        <Link
+          href={"#"}
+          className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl"
+        >
+          Success Stories
+        </Link>
+        <Link
+          href={"#"}
+          className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl"
+        >
+          Let's Connect
+        </Link>
+        <Link
+          href={"#"}
+          className="hover:bg-[#733068] hover:text-gray-50 p-3 rounded-xl"
+        >
+          Login/Register
+        </Link>
       </div>
-      {/* Mobile Screen */}
+      {/* mobile screen  */}
       <div className="block md:hidden py-3">
         {/* Logo */}
         <div className="flex items-center gap-2 px-2">
-            <Image
-            src="/Vedic.png"
-            alt="Vedic Logo"
-            width={50}
-            height={50}
-             />
-          <h1 className="flex flex-col"><span className="text-xl text-[#99408b] font-bold">Vedic Healing Center</span><span className="text-[#99408b] text-sm">Healing Through Reiki</span></h1>
+          <Image src="/Vedic.png" alt="Vedic Logo" width={50} height={50} />
+          <h1 className="flex flex-col">
+            <span className="text-xl text-[#99408b] font-bold">
+              Vedic Healing Center
+            </span>
+            <span className="text-[#99408b] text-sm">
+              Healing Through Reiki
+            </span>
+          </h1>
         </div>
         {/* Side Navbar */}
         <div className="absolute right-2 top-7">
-        <Button onClick={() => setOpenMenu((prev) => !prev)}>
-          <RiMenuFill size={60} className="text-black" />
-        </Button>
+          <Button onClick={() => setOpenMenu((prev) => !prev)}>
+            <RiMenuFill size={60} className="text-black" />
+          </Button>
         </div>
-      
+
         <div
           className={`absolute top-0 right-0 z-50 min-w-50 ${
             openMenu ? "flex" : "hidden"
@@ -80,16 +112,18 @@ const NavBar = () => {
               Let's Connect
             </Link>
             <Link href="#contact" onClick={() => setOpenMenu(false)}>
-             Login/Register
+              Login/Register
             </Link>
             {/* Button */}
             <div className="">
-        <Button className="text-base font-semibold bg-[#99408b] hover:bg-[#5c2653] text-gray-50 py-4 cursor-pointer">Book An Appointment</Button>
-      </div>
+              <Button className="text-base font-semibold bg-[#99408b] hover:bg-[#5c2653] text-gray-50 py-4 cursor-pointer">
+                Book An Appointment
+              </Button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
-export default NavBar
+  );
+};
+export default NavBar;
--- components/others/Achievement.tsx
+++ components/others/Achievement.tsx
@@ -10,94 +10,111 @@ const Achievement = () => {
     triggerOnce: true, // Ensures it only animates once
   });
   return (
-    <div className="flex flex-col md:flex-row justify-evenly items-center bg-[#dfc4db] px-2 py-4 gap-2 md:gap-6" ref={ref}>
-       {/* First item */}
+    <div
+      className="flex flex-col md:flex-row justify-evenly items-center bg-[#dfc4db] px-2 py-4 gap-2 md:gap-6"
+      ref={ref}
+    >
+      {/* First item */}
       <div className="flex items-center gap-2 py-2">
         <div>
           <Image
-          src="/icon1.png"
-          alt="First Icon"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-20 h-20"
+            src="/icon1.png"
+            alt="First Icon"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-20 h-20"
           />
         </div>
         <div>
-        <div className="text-xl md:text-2xl font-bold">
-          {inView ? <CountUp start={1} end={21} duration={5} suffix="+"  /> : "0"}
-        </div>
-        <div className="text-sm md:text-lg tracking-wider font-semibold">
-          Successful Years
-        </div>
+          <div className="text-xl md:text-2xl font-bold">
+            {inView ? (
+              <CountUp start={1} end={21} duration={5} suffix="+" />
+            ) : (
+              "0"
+            )}
+          </div>
+          <div className="text-sm md:text-lg tracking-wider font-semibold">
+            Successful Years
+          </div>
         </div>
       </div>
       {/* Second item */}
       <div className="flex items-center gap-2 py-2">
         <div>
           <Image
-          src="/icon2.png"
-          alt="Second Icon"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-20 h-20"
+            src="/icon2.png"
+            alt="Second Icon"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-20 h-20"
           />
         </div>
-      <div>
-        <div className="text-xl md:text-2xl font-bold">
-          {inView ? <CountUp start={0} end={500} duration={5} suffix="+"/> : "0"}
-        </div>
-        <div className="text-sm md:text-lg tracking-wider font-bold">
-          Happy Customer
+        <div>
+          <div className="text-xl md:text-2xl font-bold">
+            {inView ? (
+              <CountUp start={0} end={500} duration={5} suffix="+" />
+            ) : (
+              "0"
+            )}
+          </div>
+          <div className="text-sm md:text-lg tracking-wider font-bold">
+            Happy Customer
+          </div>
         </div>
       </div>
-      </div>
-     
+
       {/* Third item */}
       <div className="flex items-center gap-2 py-2">
         <div>
           <Image
-          src="/icon3.png"
-          alt="Third Icon"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-20 h-20"
+            src="/icon3.png"
+            alt="Third Icon"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-20 h-20"
           />
         </div>
         <div>
-        <div className="text-xl md:text-2xl font-bold">
-          {inView ? <CountUp start={0} end={1000} duration={5} suffix="+"/> : "0"}
+          <div className="text-xl md:text-2xl font-bold">
+            {inView ? (
+              <CountUp start={0} end={1000} duration={5} suffix="+" />
+            ) : (
+              "0"
+            )}
+          </div>
+          <div className="text-sm md:text-lg tracking-wider font-bold">
+            Healing Done
+          </div>
         </div>
-        <div className="text-sm md:text-lg tracking-wider font-bold">
-          Healing Done
-        </div>
-        </div>
-        
       </div>
       {/* Fourth Item */}
       <div className="flex items-center gap-2 py-2">
         <div>
           <Image
-          src="/icon4.png"
-          alt="Fourth Icon"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-20 h-20"
-           />
+            src="/icon4.png"
+            alt="Fourth Icon"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-20 h-20"
+          />
         </div>
         <div>
-        <div className="text-xl md:text-2xl font-bold">
-          {inView ? <CountUp start={0} end={10} duration={5} suffix="+"/> : "0"}
-        </div>
-        <div className="text-sm md:text-lg tracking-wider font-bold">
-          Team Members
+          <div className="text-xl md:text-2xl font-bold">
+            {inView ? (
+              <CountUp start={0} end={10} duration={5} suffix="+" />
+            ) : (
+              "0"
+            )}
+          </div>
+          <div className="text-sm md:text-lg tracking-wider font-bold">
+            Team Members
+          </div>
         </div>
       </div>
-        </div>
-       
     </div>
   );
 };
